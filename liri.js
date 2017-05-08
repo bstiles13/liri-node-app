b4bcef8a20c2156c39e8a4e955b3f1fa968a0a04
Brandon Stiles <brandonstiles@Brandons-MacBook-Pro.local>
2017-05-07 19:07:29 -0700

    added comments

--- liri.js
+++ liri.js
@@ -1,9 +1,14 @@
+//When node is used on the command line:
+//"search" will determine the api to call
+//"name" will give the api a parameter
 var search = process.argv[2];
 var name = process.argv[3];
 
+//Initiating Request and File System modules
 var request = require('request');
 var fs = require('fs');
 
+//If the user calls "do-what-it-says" at argv[2] in the command line, the random.txt file will determine the api
 if (search === "do-what-it-says") {
     fs.readFile('random.txt', 'utf8', function(err, res) {
         console.log(res);
@@ -12,9 +17,11 @@ if (search === "do-what-it-says") {
         name = split[1];
         start();
     });
+    //Otherwise compare argv[2] to three possible cases (twitter, spotify, or omdb) in the start function
 } else {
     start();
 }
+
 
 function start() {
     switch (search) {
@@ -32,51 +39,62 @@ function start() {
     }
 }
 
+//Initiate Spotify api when "spotify-this-song" case is called in terminal
 function spotify() {
     var track = name;
-    console.log(track);
+
+    //If a track is not specified at argv[3], search parameters default to The Sign by Ace of Base
     if (name === undefined) {
-    	track = "the+sign";
-    	var artist = "ace+of+base";
+        track = "the+sign";
+        var artist = "ace+of+base";
     } else {
-    	var artist = "";
+        var artist = "";
     }
-    console.log(track);
-    console.log(artist);
+
+    //The object that tells Spotify what track to return information for
     var options = {
         url: 'https://api.spotify.com/v1/search?q=track:' + track + '%20artist:' + artist + '&type=track&limit=1',
         method: 'GET'
     };
     // console.log(JSON.stringify(options));
 
+    //Sending request to Spotify and storing subsequent callback data
     request(options, function(err, res, body) {
         var json = JSON.parse(body);
         // console.log(body);
+        //Filtering specific callback data into variables
         var artist = 'Artist: ' + json.tracks.items[0].artists[0].name;
         var track = 'Track: ' + json.tracks.items[0].name;
         var preview = 'Preview: ' + json.tracks.items[0].external_urls.spotify;
         var album = 'Album: ' + json.tracks.items[0].album.name;
 
+        //Calling varibles in terminal
         console.log(artist);
         console.log(track);
         console.log(preview);
         console.log(album);
+
+        //Appending data to log.txt
         fs.appendFile('log.txt', '\n' + artist + '\n' + track + '\n' + preview + '\n' + album + '\n');
     })
 }
 
+//Initiate OMDB api when "movie-this" case is called in terminal
 function movie() {
     var movie = name;
 
+    //The object that tells OMDB what movie to return information for
     var options = {
         url: 'http://www.omdbapi.com/?t=' + movie,
         method: 'GET'
     };
 
+    //Sending request to OMDB and storing subsequent callback data
     request(options, function(err, res, body) {
         var json = JSON.parse(body);
 
         // console.log(body);
+        //Filtering specific callback data into variables
         var title = 'Title: ' + json.Title;
         var year = 'Year: ' + json.Year;
         var rating = 'IMDB Rating: ' + json.imdbRating;
@@ -86,6 +104,7 @@ function movie() {
         var actors = 'Actors: ' + json.Actors;
         var rotten = 'Rotten Tomatoes: ' + json.Ratings[1].Value;
 
+        //Calling varibles in terminal
         console.log(title);
         console.log(year);
         console.log(rating);
@@ -94,24 +113,31 @@ function movie() {
         console.log(plot);
         console.log(actors);
         console.log(rotten);
+
+        //Appending data to log.txt
         fs.appendFile('log.txt', '\n' + title + '\n' + year + '\n' + rating + '\n' + country + '\n' + language + '\n' + plot + '\n' + actors + '\n' + rotten + '\n');
     })
 
 
 }
 
+//Initiate Twitter api when "my-tweets" case is called in terminal
 function twitter() {
     var Twitter = require('twitter');
     var keys = require('./keys.js');
 
+    //The object that tells Twitter what movie to return information for    
     var client = new Twitter(keys.twitterKeys);
 
     var params = {
         screen_name: 'brandonthecoder'
     };
+
+    //Sending request to Twitter and storing subsequent callback data
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error) {
             // console.log(tweets);
+            //Console logging and appending all tweets under 'brandonthecoder' up to 20 tweet limit
             for (var i = 0; i < Math.min(tweets.length, 19); i++) {
                 var date = 'Tweet #' + (i + 1) + ' - ' + tweets[i].created_at;
                 var tweet = tweets[i].text;
